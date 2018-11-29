35531f13c125a263214ebbd1cecd764ff248ec52
Seth Bartlett <bartlett.seth@gmail.com>
2018-11-29 11:53:16 -0500

    Fuck me sideways

--- comm.c
+++ comm.c
@@ -2288,7 +2288,7 @@ void act(const char *format, CHAR_DATA *ch, const void *arg1, const void *arg2, 
 		str = format;
 		while (*str != '\0')
 		{
-			if (*str != '$' || *str != '`')
+			if (*str != '$' )
 			{
 				*point++ = *str++;
 				continue;
