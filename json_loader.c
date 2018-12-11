61dfa2e052f12437d6f5292ebebb74856d32cb56
Seth Bartlett <bartlett.seth@gmail.com>
2018-12-11 00:22:21 -0500

    Didnt change it on the jsonloader

--- json_loader.c
+++ json_loader.c
@@ -778,7 +778,7 @@ void load_area_file_json(char *areaFile)
         log_string("Loading reset");
         pReset = alloc_perm(sizeof(*pReset));
         pReset->command = str_dup(cJSON_GetObjectItemCaseSensitive(reset, "command")->valuestring)[0];
-        pReset->description = str_dup(cJSON_GetObjectItemCaseSensitive(reset, "description")->valuestring);
+        pReset->comment = str_dup(cJSON_GetObjectItemCaseSensitive(reset, "comment")->valuestring);
         pReset->arg1 = cJSON_GetObjectItemCaseSensitive(reset, "arg1")->valuedouble;
         pReset->arg2 = cJSON_GetObjectItemCaseSensitive(reset, "arg2")->valuedouble;
         pReset->arg3 = cJSON_GetObjectItemCaseSensitive(reset, "arg3")->valuedouble;
