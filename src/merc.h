fd23774d80cfde4e3d3ddced4ce8d69e0399011c
Palmer - Home <palmermarc@gmail.com>
2019-03-09 18:01:18 -0600

    Commented out a second newbie channel

--- src/merc.h
+++ src/merc.h
@@ -1337,7 +1337,7 @@ extern char * const stancenames[11];
 #define  CHANNEL_YELL		128
 #define  CHANNEL_VAMPTALK	256
 #define  CHANNEL_IMMINFO	512
-#define  CHANNEL_NEWBIE		1024
+//#define  CHANNEL_NEWBIE		1024
 #define  CHANNEL_PERSONAL	2048
 #define  CHANNEL_MCHAT		4096
 #define  CHANNEL_ASSTALK    8192
