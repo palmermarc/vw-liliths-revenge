8751fe76e94fd6a145e21dc7521394992ab71a67
palmermarc <palmermarc@gmail.com>
2019-03-11 15:36:11 -0500

    More typos that were causing warnings

--- src/clandiscs.c
+++ src/clandiscs.c
@@ -881,7 +881,7 @@ void do_contortion(CHAR_DATA *ch, CLANDISC_DATA *disc, char *argument)
             snprintf(buf, MAX_INPUT_LENGTH, "You contort %s's limbs and rot away their left arm.", victim->name);
             disc->personal_message_on = str_dup(buf);
 
-            snprint(buf, MAX_INPUT_LENGTH, "$n contorts your body and it rots away your left arm.");
+            snprintf(buf, MAX_INPUT_LENGTH, "$n contorts your body and it rots away your left arm.");
             disc->victim_message = str_dup(buf);
 
         }
@@ -917,7 +917,7 @@ void do_contortion(CHAR_DATA *ch, CLANDISC_DATA *disc, char *argument)
             snprintf(buf, MAX_INPUT_LENGTH, "You contort %s's limbs and rot away their right arm.", victim->name);
             disc->personal_message_on = str_dup(buf);
 
-            snprint(buf, MAX_INPUT_LENGTH, "$n contorts your body and it rots away your right arm.");
+            snprintf(buf, MAX_INPUT_LENGTH, "$n contorts your body and it rots away your right arm.");
             disc->victim_message = str_dup(buf);
         }
         else if(location == 3)
@@ -946,7 +946,7 @@ void do_contortion(CHAR_DATA *ch, CLANDISC_DATA *disc, char *argument)
             snprintf(buf, MAX_INPUT_LENGTH, "You contort %s's limbs and rot away their right leg.", victim->name);
             disc->personal_message_on = str_dup(buf);
 
-            snprint(buf, MAX_INPUT_LENGTH, "$n contorts your body and it rots away your right leg.");
+            snprintf(buf, MAX_INPUT_LENGTH, "$n contorts your body and it rots away your right leg.");
             disc->victim_message = str_dup(buf);
         }
         else
@@ -975,7 +975,7 @@ void do_contortion(CHAR_DATA *ch, CLANDISC_DATA *disc, char *argument)
             snprintf(buf, MAX_INPUT_LENGTH, "You contort %s's limbs and rot away their left leg.", victim->name);
             disc->personal_message_on = str_dup(buf);
 
-            snprint(buf, MAX_INPUT_LENGTH, "$n contorts your body and it rots away your left leg.");
+            snprintf(buf, MAX_INPUT_LENGTH, "$n contorts your body and it rots away your left leg.");
             disc->victim_message = str_dup(buf);
         }
 
@@ -996,7 +996,7 @@ void do_contortion(CHAR_DATA *ch, CLANDISC_DATA *disc, char *argument)
         snprintf(buf, MAX_INPUT_LENGTH, "Your contortion attempt has failed.\n\r");
         disc->personal_message_on = str_dup(buf);
 
-        snprintf(buf, MAX_INPUT_LENGTH, "$n has tried to sever your limbs and failed.\n\r", ch->name);
+        snprintf(buf, MAX_INPUT_LENGTH, "$n has tried to sever your limbs and failed.\n\r");
         disc->victim_message = str_dup(buf);
 
         do_clandisc_message(ch, NULL, disc);
