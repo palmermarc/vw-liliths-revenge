8298094393fc2c028d01e62717c6cb102c8e6c7c
palmermarc <palmermarc@gmail.com>
2019-03-12 11:13:00 -0500

    There are too many "fist" commands

--- src/clandiscs.c
+++ src/clandiscs.c
@@ -510,6 +510,8 @@ void do_crush(CHAR_DATA *ch, CLANDISC_DATA *disc, char *argument)
 
 void do_the_fist_of_lillith(CHAR_DATA *ch, CLANDISC_DATA *disc, char *argument) 
 {
+    char buf[MAX_INPUT_LENGTH];
+
     if (!IS_SET(ch->act, PLR_VAMPIRE) || disc == NULL)
     {
         send_to_char("You are unable to perform that action.\n\r", ch);
@@ -559,8 +561,6 @@ void do_the_forgers_hammer(CHAR_DATA *ch, CLANDISC_DATA *disc, char *argument)
 
 void do_fist_of_the_titans(CHAR_DATA *ch, CLANDISC_DATA *disc, char *argument) 
 {
-    char buf[MAX_INPUT_LENGTH];
-    
     if (!IS_SET(ch->act, PLR_VAMPIRE) || disc == NULL)
     {
         send_to_char("You are unable to perform that action.\n\r", ch);
