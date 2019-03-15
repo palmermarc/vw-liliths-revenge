56490701b3ac90e9a6490edbacae50bf0a90bcf3
Palmer - Home <palmermarc@gmail.com>
2019-03-14 21:24:45 -0500

    Removing the test line

--- src/act_info.c
+++ src/act_info.c
@@ -4869,8 +4869,8 @@ int Get_Hitroll_Bonus(CHAR_DATA *ch)
             hitrollBonus += (int) (ch->hitroll * 0.25);
         }
     }
-    snprintf(buf, MAX_INPUT_LENGTH, "\n\r\n\rYour bonus hitroll is %d.\n\r\n\r", hitrollBonus);
-    send_to_char(buf, ch);
+    //snprintf(buf, MAX_INPUT_LENGTH, "\n\r\n\rYour bonus hitroll is %d.\n\r\n\r", hitrollBonus);
+    //send_to_char(buf, ch);
 
     return hitrollBonus;
 }
