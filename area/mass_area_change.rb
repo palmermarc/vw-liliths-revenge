a9edbc8a3ec0cd169b5eec532bf616f921be948d
unknown <bartlett.seth@gmail.com>
2019-03-26 17:04:41 -0400

    Updated ruby script and starting areas

--- area/mass_area_change.rb
+++ area/mass_area_change.rb
@@ -1,5 +1,6 @@
 require "json"
 
+# Starting area files
 files = ["draconia.json", "elysium.json", "midennir.json", "moria.json", "ofcol2.json", "smurf.json", "thalos.json"]
 
 def SetMinMax(object, min, max)
@@ -12,8 +13,7 @@ files.each do |fileName|
     data = JSON.parse(file)
 
     data["Objects"].each do |i|
-        case i["Item_Type"]
-        when 5
+        if i["Item_Type"] === 5
             i["Affect_Data"].each do |a|
                 case a["Location"]
                 when 18, 19
@@ -22,18 +22,20 @@ files.each do |fileName|
                     SetMinMax a, 5, 13
                 end
             end
-        when 9
+        end
+        if i["Item_Type"] ===  9
             i["Affect_Data"].each do |a|
                 case a["Location"]
                 when 1..5
                     SetMinMax a, -3, 3
                 when 12, 14
-                    SetMinMax a, 5, 19
+                    SetMinMax a, 7, 19
                 when 13
                     SetMinMax a, 8, 22
                 end
             end
-        when 37
+        end
+        if i["Item_Type"] === 37
             if i["Wear_Flags"].include? 8
                 i["Affect_Data"].each do |a|
                     case a["Location"]
@@ -83,7 +85,8 @@ files.each do |fileName|
                     end
                 end
             end
-        when 38
+        end
+        if i["Item_Type"] === 38
             if i["Wear_Flags"].include? 8
                 i["Affect_Data"].each do |a|
                     case a["Location"]
@@ -132,7 +135,8 @@ files.each do |fileName|
                     end
                 end
             end
-        when 39
+        end
+        if i["Item_Type"] === 39
             if i["Wear_Flags"].include? 8
                 i["Affect_Data"].each do |a|
                     case a["Location"]
@@ -181,7 +185,8 @@ files.each do |fileName|
                     end
                 end
             end
-        when 37, 38, 39
+        end
+        if [37, 38, 39].include? i["Item_Type"]
             if i["Wear_Flags"].include? 128
                 i["Affect_Data"].each do |a|
                     case a["Location"]
@@ -228,7 +233,8 @@ files.each do |fileName|
                     end
                 end
             end
-        when 40
+        end
+        if i["Item_Type"] === 40
             i["Affect_Data"].each do |a|
                 case a["Location"]
                 when 18, 19
@@ -237,7 +243,8 @@ files.each do |fileName|
                     SetMinMax a, 12, 30
                 end
             end
-        when 41
+        end
+        if i["Item_Type"] === 41
             i["Affect_Data"].each do |a|
                 case a["Location"]
                 when 18, 19
@@ -246,7 +253,8 @@ files.each do |fileName|
                     SetMinMax a, 5, 13
                 end
             end
-        when 42
+        end
+        if i["Item_Type"] === 42
             i["Affect_Data"].each do |a|
                 case a["Location"]
                 when 1, 5
