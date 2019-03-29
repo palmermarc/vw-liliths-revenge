beefd3a172bdf74b885a2a7de69bdc44cfe3bea5
unknown <bartlett.seth@gmail.com>
2019-03-29 14:53:36 -0400

    Trying a new macro

--- src/fight.c
+++ src/fight.c
@@ -1346,9 +1346,7 @@ bool check_block(CHAR_DATA *ch, CHAR_DATA *victim, int dt)
     if (!IS_AWAKE(victim))
         return FALSE;
 
-    obj = get_eq_char(victim, WEAR_HOLD);
-
-    if (!IS_SHIELD(obj))
+    if (!IS_SHIELD(obj = get_eq_char(victim, WEAR_HOLD)))
         return FALSE;
 
     for ( paf = obj->affected; paf != NULL; paf = paf->next )
