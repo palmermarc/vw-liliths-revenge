de6dc495505dd22837069856415f12a588883c4a
Palmer - Home <palmermarc@gmail.com>
2019-03-30 21:25:22 -0500

    Updated damage on fire breath and fixed the ignite chance now that testing was successful

--- src/magic.c
+++ src/magic.c
@@ -2688,6 +2688,8 @@ void spell_fire_breath(int sn, int level, CHAR_DATA *ch, void *vo)
     int hpch;
     int counter = 0;
 
+    dam = skill_table[sn].basedmg;
+
     for (vch = ch->in_room->people; vch != NULL; vch = vch_next)
     {
         // Increased the amount of things hit to 12
@@ -2707,20 +2709,17 @@ void spell_fire_breath(int sn, int level, CHAR_DATA *ch, void *vo)
             hpch = UMAX(10, ch->hit);
             if (!IS_NPC(ch) && ch->max_mana >= 1000)
             {
-                dam = number_range(ch->max_mana / 16, ch->max_mana / 8);
-                if (dam > 1250)
-                    dam = 1250;
+                dam += number_range(ch->max_mana / 90, ch->max_mana / 110);
 
                 if (!IS_NPC(ch) && ch->spl[SPELL_PURPLE] >= 200 && ch->spl[SPELL_RED] >= 200 && ch->spl[SPELL_BLUE] >= 200 && ch->spl[SPELL_GREEN] >= 200 && ch->spl[SPELL_YELLOW] >= 200)
                 {
-                    dam *= 1.25; // GS all bonus, 50% damage increase
-
-                    //
-                    if (number_range(1, 100) > 85)
+                    if (number_percent() > 85)
                     {
-                        dam += (number_range(50, 500));
+                        dam += (number_range(250, 500));
                         send_to_char("Your skin sparks with magical energy.\n\r", ch);
                     }
+
+                    dam *= 1.25; // GS all bonus, 50% damage increase
                 }
 
                 if( ch->remortlevel > 0 )
@@ -2739,7 +2738,7 @@ void spell_fire_breath(int sn, int level, CHAR_DATA *ch, void *vo)
 
             damage(ch, vch, dam, sn);
 
-            if( number_percent() > 1 ) // You "crit" - set them on fire
+            if( number_percent() > 95 ) // You "crit" - set them on fire
             {
                 af.type = sn;
                 af.duration = level;
@@ -2751,7 +2750,7 @@ void spell_fire_breath(int sn, int level, CHAR_DATA *ch, void *vo)
                 snprintf(buf, MAX_INPUT_LENGTH, "Your fire breath has set %s on fire!.\n\r", vch->short_descr);
                 send_to_char(buf, ch);
             }
-            
+
             if (!IS_NPC(vch) && IS_SET(vch->act, PLR_VAMPIRE) && vch->hit <= ((vch->max_hit) - dam))
                 vch->hit = vch->hit + (dam / 4);
 
