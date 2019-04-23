bbe337a16183fb858941d4934fa2246fef5027f0
senial <45372209+senial@users.noreply.github.com>
2019-04-23 13:34:38 -0400

    Ruby files

---
+++ area/20k_areas.rb
@@ -0,0 +1,268 @@
+require "json"
+
+# Area Files
+files = ["tanu.json", "ultima.json"]
+
+def SetMinMax(object, min, max)
+    object["Min_modifier"] = min
+    object["Max_modifier"] = max
+  end
+
+files.each do |fileName|
+    file = File.read(fileName)
+    data = JSON.parse(file)
+
+    data["Objects"].each do |i|
+        if i["Item_Type"] === 5
+            i["Affect_Data"].each do |a|
+                case a["Location"]
+                when 18
+                    SetMinMax a, 330, 908
+                when 19
+                    SetMinMax a, 166, 455
+                when 28
+                    SetMinMax a, 5, 13
+                end
+            end
+        end
+        if i["Item_Type"] === 9
+            if i["Wear_Flags"].include? 2 or i["Wear_Flags"].include? 4
+                i["Affect_Data"].each do |a|
+                    case a["Location"]
+                    when 1..5
+                        SetMinMax a, 14, 37
+                    when 12, 14
+                        SetMinMax a, 134, 367
+                    when 13
+                        SetMinMax a, 160, 440
+                    when 24
+                        SetMinMax a, 0, 0
+                    end
+                end
+            end
+            if i["Wear_Flags"].include? 1024 or i["Wear_Flags"].include? 2048
+                i["Affect_Data"].each do |a|
+                    case a["Location"]
+                    when 1..5
+                        SetMinMax a, 14, 37
+                    when 12, 14
+                        SetMinMax a, 134, 367
+                    when 13
+                        SetMinMax a, 160, 440
+                    end
+                end
+            end
+        end
+        if i["Item_Type"] === 37
+            if i["Wear_Flags"].include? 8
+                i["Affect_Data"].each do |a|
+                    case a["Location"]
+                    when 1, 2, 5
+                        SetMinMax a, 14, 37
+                    when 13
+                        SetMinMax a, 160, 440
+                    when 30
+                        SetMinMax a, 2034, 4746
+                    end
+                end
+            end
+            if i["Wear_Flags"].include? 16
+                i["Affect_Data"].each do |a|
+                    case a["Location"]
+                    when 3
+                        SetMinMax a, 14, 37
+                    when 12
+                        SetMinMax a, 134, 367
+                    when 30
+                        SetMinMax a, 2034, 4746
+                    end
+                end
+            end
+
+            if i["Wear_Flags"].include? 32 or i["Wear_Flags"].include? 64
+                i["Affect_Data"].each do |a|
+                    case a["Location"]
+                    when 2
+                        SetMinMax a, 14, 37
+                    when 14
+                        SetMinMax a, 134, 367
+                    when 30
+                        SetMinMax a, 2034, 4746
+                    end
+                end
+            end
+        end
+        if i["Item_Type"] === 38
+            if i["Wear_Flags"].include? 8
+                i["Affect_Data"].each do |a|
+                    case a["Location"]
+                    when 1, 2, 5
+                        SetMinMax a, 14, 37
+                    when 13
+                        SetMinMax a, 160, 440
+                    when 17, 30
+                        SetMinMax a, 1017, 2373
+                    end
+                end
+            end
+            if i["Wear_Flags"].include? 16
+                i["Affect_Data"].each do |a|
+                    case a["Location"]
+                    when 3
+                        SetMinMax a, 14, 37
+                    when 12
+                        SetMinMax a, 134, 367
+                    when 17, 30
+                        SetMinMax a, 1017, 2373
+                    end
+                end
+            end
+            if i["Wear_Flags"].include? 32 or i["Wear_Flags"].include? 64
+                i["Affect_Data"].each do |a|
+                    case a["Location"]
+                    when 2
+                        SetMinMax a, 14, 37
+                    when 14
+                        SetMinMax a, 134, 367
+                    when 17, 30
+                        SetMinMax a, 1017, 2373
+                    end
+                end
+            end
+        end
+        if i["Item_Type"] === 39
+            if i["Wear_Flags"].include? 8
+                i["Affect_Data"].each do |a|
+                    case a["Location"]
+                    when 1, 2, 5
+                        SetMinMax a, 14, 37
+                    when 13
+                        SetMinMax a, 160, 440
+                    when 17
+                        SetMinMax a, 2034, 4746
+                    end
+                end
+            end
+            if i["Wear_Flags"].include? 16
+                i["Affect_Data"].each do |a|
+                    case a["Location"]
+                    when 3
+                        SetMinMax a, 14, 37
+                    when 12
+                        SetMinMax a, 134, 367
+                    when 17
+                        SetMinMax a, 2034, 4746
+                    end
+                end
+            end
+            if i["Wear_Flags"].include? 32 or i["Wear_Flags"].include? 64
+                i["Affect_Data"].each do |a|
+                    case a["Location"]
+                    when 2
+                        SetMinMax a, 14, 37
+                    when 14
+                        SetMinMax a, 134, 367
+                    when 17
+                        SetMinMax a, 2034, 4746
+                    end
+                end
+            end
+        end
+        if [37, 38, 39].include? i["Item_Type"]
+            if i["Wear_Flags"].include? 128
+                i["Affect_Data"].each do |a|
+                    case a["Location"]
+                    when 1
+                        SetMinMax a, 14, 37
+                    when 18
+                        SetMinMax a, 248, 681
+                    when 19
+                        SetMinMax a, 124, 340
+                    when 28
+                        SetMinMax a, 4, 10
+                    end
+                end
+            end
+            if i["Wear_Flags"].include? 256
+                i["Affect_Data"].each do |a|
+                    case a["Location"]
+                    when 1, 5
+                        SetMinMax a, 14, 37
+                    when 18
+                        SetMinMax a, 248, 681
+                    when 19
+                        SetMinMax a, 124, 340
+                    end
+                end
+            end
+            if i["Wear_Flags"].include? 4096
+                i["Affect_Data"].each do |a|
+                    case a["Location"]
+                    when 1
+                        SetMinMax a, 14, 37
+                    when 18
+                        SetMinMax a, 248, 681
+                    when 19
+                        SetMinMax a, 124, 340
+                    when 28
+                        SetMinMax a, 4, 10
+                    end
+                end
+            end
+            if i["Wear_Flags"].include? 32768
+                i["Affect_Data"].each do |a|
+                    case a["Location"]
+                    when 3
+                        SetMinMax a, 14, 37
+                    when 12
+                        SetMinMax a, 134, 367
+                    when 18
+                        SetMinMax a, 248, 681
+                    when 19
+                        SetMinMax a, 124, 340
+                    end
+                end
+            end
+        end
+        if i["Item_Type"] === 40
+            i["Affect_Data"].each do |a|
+                case a["Location"]
+                when 18
+                    SetMinMax a, 990, 2723
+                when 19
+                    SetMinMax a, 496, 1363
+                when 28
+                    SetMinMax a, 12, 30
+                end
+            end
+        end
+        if i["Item_Type"] === 41
+            i["Affect_Data"].each do |a|
+                case a["Location"]
+                when 18
+                    SetMinMax a, 330, 908
+                when 19
+                    SetMinMax a, 166, 455
+                when 28
+                    SetMinMax a, 5, 13
+                end
+            end
+        end
+        if i["Item_Type"] === 42
+            i["Affect_Data"].each do |a|
+                case a["Location"]
+                when 1, 5
+                    SetMinMax a, 14, 37
+                when 17
+                    SetMinMax a, 2543, 5085
+                when 29
+                    SetMinMax a, 10, 45
+                end
+            end
+        end
+    end
+
+    File.open(fileName, "w") do |f|
+      f.write(JSON.pretty_generate(data))
+    end
+end
