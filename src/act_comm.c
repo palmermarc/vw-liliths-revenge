a588d48aeba6914ffa1f3d3b54eefe3f8310139f
Seth Bartlett <bartlett.seth@gmail.com>
2019-04-24 14:20:21 -0400

    Testing discord chat message

--- src/act_comm.c
+++ src/act_comm.c
@@ -408,6 +408,7 @@ void talk_channel(CHAR_DATA *ch, char *argument, int channel, const char *verb)
 {
 	char buf[MAX_STRING_LENGTH];
 	char buf2[MAX_STRING_LENGTH];
+	char discordBuf[MAX_STRING_LENGTH];
 	DESCRIPTOR_DATA *d;
 	int position;
 
@@ -460,6 +461,8 @@ void talk_channel(CHAR_DATA *ch, char *argument, int channel, const char *verb)
 	case CHANNEL_CHAT:
 		snprintf(buf, MAX_STRING_LENGTH, "#lYou %s '%s'.\n\r", verb, argument);
 		send_to_char(buf, ch);
+		snprintf(discordBuf, MAX_STRING_LENGTH, "'{\"username\": \"%s\", \"content\": \"%s\"}'", ch, argument);
+		exec.Command("curl", "-H", "\"Content-Type: application/json\"", "-X", "POST", "-d", discordBuf, "https://discordapp.com/api/webhooks/570668388557389841/rzjV2IZfHqp7F29cRzzABrNh1Yir_BhcwWIxkday8DvAp_SGQihtQf48zLi49uy-zxVh");
 		if(ch->pcdata != NULL)
 		{
 			add_to_history(ch->pcdata->chat_history, buf);
