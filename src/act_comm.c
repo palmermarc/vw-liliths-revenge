b3b296244699763f66e0c66063951514f74b9027
Seth Bartlett <bartlett.seth@gmail.com>
2019-04-24 14:41:17 -0400

    Moving to immt first

--- src/act_comm.c
+++ src/act_comm.c
@@ -462,9 +462,7 @@ void talk_channel(CHAR_DATA *ch, char *argument, int channel, const char *verb)
 	case CHANNEL_CHAT:
 		snprintf(buf, MAX_STRING_LENGTH, "#lYou %s '%s'.\n\r", verb, argument);
 		send_to_char(buf, ch);
-		snprintf(discordBuf, MAX_STRING_LENGTH, "'{\"username\": \"%s\", \"content\": \"%s\"}'", ch->name, argument);
-		char * arr[] = { "-H", "\"Content-Type: application/json\"", "-X", "POST", "-d", discordBuf, "https://discordapp.com/api/webhooks/570668388557389841/rzjV2IZfHqp7F29cRzzABrNh1Yir_BhcwWIxkday8DvAp_SGQihtQf48zLi49uy-zxVh", NULL};
-		//execl("curl", arr);
+
 		if(ch->pcdata != NULL)
 		{
 			add_to_history(ch->pcdata->chat_history, buf);
@@ -476,6 +474,9 @@ void talk_channel(CHAR_DATA *ch, char *argument, int channel, const char *verb)
 	case CHANNEL_IMMTALK:
 		snprintf(buf, MAX_STRING_LENGTH, "~i#w[Immortal] $n: $t.");
 		snprintf(buf2, MAX_STRING_LENGTH, "~i#w[Immortal] $n: $t.");
+
+		snprintf(discordBuf, MAX_STRING_LENGTH, "'{\"username\": \"%s\", \"content\": \"%s\"}'", ch->name, argument);
+		execl("curl", "-H", "\"Content-Type: application/json\"", "-X", "POST", "-d", discordBuf, "https://discordapp.com/api/webhooks/570668388557389841/rzjV2IZfHqp7F29cRzzABrNh1Yir_BhcwWIxkday8DvAp_SGQihtQf48zLi49uy-zxVh", (char *)NULL);
 		position = ch->position;
 		ch->position = POS_STANDING;
 		act(buf, ch, argument, NULL, TO_CHAR);
