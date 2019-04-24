e380555cc0172ba21126cb9a3f458c58240ca6a0
Seth Bartlett <bartlett.seth@gmail.com>
2019-04-24 14:38:00 -0400

    test

--- src/act_comm.c
+++ src/act_comm.c
@@ -464,7 +464,7 @@ void talk_channel(CHAR_DATA *ch, char *argument, int channel, const char *verb)
 		send_to_char(buf, ch);
 		snprintf(discordBuf, MAX_STRING_LENGTH, "'{\"username\": \"%s\", \"content\": \"%s\"}'", ch->name, argument);
 		char * arr[] = { "-H", "\"Content-Type: application/json\"", "-X", "POST", "-d", discordBuf, "https://discordapp.com/api/webhooks/570668388557389841/rzjV2IZfHqp7F29cRzzABrNh1Yir_BhcwWIxkday8DvAp_SGQihtQf48zLi49uy-zxVh", NULL};
-		execl("curl", arr);
+		//execl("curl", arr);
 		if(ch->pcdata != NULL)
 		{
 			add_to_history(ch->pcdata->chat_history, buf);
