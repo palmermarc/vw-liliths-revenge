9b6de2dd01429c63919e37873dcca2f9369e4d62
Palmer - Home <palmermarc@gmail.com>
2019-06-18 18:19:08 -0500

    Another fix

--- src/save.c
+++ src/save.c
@@ -535,7 +535,7 @@ void fwrite_obj(CHAR_DATA *ch, OBJ_DATA *obj, cJSON *objects , int iNest)
 	for (ed = obj->extra_descr; ed != NULL; ed = ed->next)
 	{
 	    extra_description = cJSON_CreateObject();
-        cJSON_AddItemToObject(extra_descriptions, extra_description);
+        cJSON_AddItemToArray(extra_descriptions, extra_description);
 		cJSON_AddItemToObject(extra_description, "keyword", cJSON_CreateString(ed->keyword));
 		cJSON_AddItemToObject(extra_description, "description", cJSON_CreateString(ed->description));
 	}
