e8564fc906520a122cf34875c703ce39e9063a45
Palmer - Home <palmermarc@gmail.com>
2019-06-19 21:47:31 -0500

    I'm too old for missing all of these semicolons.. It's getting close to bed time.

--- src/save.c
+++ src/save.c
@@ -958,7 +958,7 @@ bool load_char_obj(DESCRIPTOR_DATA *d, char *name)
 
         load_char_clandiscs_json(cJSON_GetObjectItemCaseSensitive(jChar, "clandiscs"), ch);
 
-		load_char_objects_json(cJSON_GetObjectItemCaseSensitive(jChar, "objects"), ch)
+		load_char_objects_json(cJSON_GetObjectItemCaseSensitive(jChar, "objects"), ch);
 
         load_char_affects_json(cJSON_GetObjectItemCaseSensitive(jChar, "affect_data"), ch);
 
